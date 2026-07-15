01b5ba6a28c174cd7562de0ea5c4dc00b64cc263
ilyamorozov207-ship-it <ilyamorozov207@gmail.com>
2026-07-15 17:03:31 +0500

    Delete menu

--- SITE.py
+++ SITE.py
@@ -2,22 +2,7 @@ from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
-HTML = """
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <title>Мой сайт</title>
-</head>
-<body>
 
-<header style="text-align:center; padding:50px; background:#2563eb; color:white;">
-    <h1>Мой сайт</h1>
-    <p>Учебный проект на Flask</p>
-</header>
-
-</body>
-</html>
-"""
 
 @app.route("/")
 def home():
